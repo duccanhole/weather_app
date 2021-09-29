a52c4f5d8e8ced0a7b1d30d591d976a52e885803
Canh Duc <62925811+duccanhole@users.noreply.github.com>
2021-09-29 20:55:28 +0700

    Update get.js
    change http to https

--- get.js
+++ get.js
@@ -7,7 +7,7 @@ function getData(){
     let cityName = document.getElementById('cityname').value;
     let url = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=8dd3399efb163e5a7a5f94b9359f035c';
     return url;
-}
+} 
 function getApi(){
     fetch(getData())
     .then(res=>res.json())
